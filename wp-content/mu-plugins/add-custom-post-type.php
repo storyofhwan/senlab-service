76c94ca5083e924d03b9aadd6bc59e945940c004
storyofhwan <storyofhwan@gmail.com>
2017-09-23 18:51:45 +0900

    first commit

---
+++ wp-content/mu-plugins/add-custom-post-type.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Add Custom Post type
+Plugin URI: https://akismet.com/
+Description: Talent, Company post type을 추가합니다.
+Version: 0.0.1
+Author: 최명환
+Author URI: 
+*/
---
+++ wp-content/plugins/senlab-matching/SENLab-matching.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: SENLab Matching
+Plugin URI: https://senlab.co.kr/
+Description: 학생과 기업간의 wish, matching, interview에 관한 기능을 담당하는 plugin입니다.
+Version: 0.0.1
+Author: 최명환
+Author URI: https://facebook.co.kr/storyofhwan
+License: 
+Text Domain: senlab
+*/
